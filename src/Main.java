b9aa03f874e426064c4223cb8871f0e2f9cb4c5b
Renata Akhmetzyanova <are.ru@inbox.ru>
2023-03-23 13:04:53 +0300

    Добавлено решение потери выручки

--- src/Main.java
+++ src/Main.java
@@ -57,7 +57,54 @@ public class Main {
         }
 
         double ro_och = (Math.pow(ro,n)/getFactorial(n))*((1-Math.pow(ro/n,m))/(1-(ro/n))*ro_0);
-        System.out.println("Вероятность образования очереди = " + ro_och*100);
+        System.out.println("Вероятность образования очереди = " + ro_och);
+
+        double ver_otkaza = ((Math.pow(ro,n)/getFactorial(n))*ro_0);
+        System.out.println("Вероятность отказа в обслуживании = " + ver_otkaza);
+
+        double prop_spos = 1 - ver_otkaza;
+        System.out.println("Относительная пропускная способность = " + prop_spos);
+
+        double abs_prop = 12*prop_spos;
+        System.out.println("Абсолютная пропускная способность = " + abs_prop);
+
+        double av_busy = ro*prop_spos;
+        System.out.println("Среднее число занятых каналов = " + av_busy);
+
+        double av_chan = n - av_busy;
+        System.out.println("Среднее число простаивающих каналов = " + av_chan);
+
+        double koef = av_busy/n;
+        System.out.println("Коэффициент занятости = " + koef);
+
+        double av_zay_och = ((Math.pow(ro, n+1))/(n*getFactorial(n)*((m*(m+1))/2)*ro_0));
+        System.out.println("Среднее число заявок в очереди = " + av_zay_och);
+
+        double av_t = av_zay_och/n;
+        System.out.println("Среднее время ожидания в очереди = " + av_t);
+
+        double av_t_preb_zay = (av_zay_och/n)+(prop_spos/3);
+        System.out.println("Среднее время пребывания заявки в системе = " + av_t_preb_zay);
+
+        double nom_proiz = n/(20.0/60.0);
+        System.out.println("Номинальная производительность = " + nom_proiz);
+
+
+        double fact_proiz = abs_prop/nom_proiz;
+        System.out.println("Фактическая производительность = " + fact_proiz*100);
+
+        System.out.println(" _________________________ ");
+
+        double day = 10;
+        double otkaz_hour = 12*ver_otkaza;
+        System.out.println("Число заявок, получивших отказ в течение часа = " + otkaz_hour);
+
+        double otkaz_day = otkaz_hour*day;
+        System.out.println("Число заявок, получивших отказ с 9 до 19 часов = " + otkaz_day);
+
+        double clean_cost = 150;
+        double lost_money = otkaz_day*clean_cost;
+        System.out.println("Средняя величина потери выручки = " + lost_money);
 
 
     }
